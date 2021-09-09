0083d05d1882973b5db662da5cd40a06797476b3
unknown <rajputnikita5666@gmail.com>
2021-09-09 21:58:13 +0530

    [Nikita] Adding Coupon Number program

---
+++ CouponNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class CouponNumber
+    {
+        public static void CouponNumber1()
+        {
+            int i;
+            int randomCount = 0;
+            int ran = 0;
+            Console.WriteLine("Enter no of distinct coupon numbers");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intDistinct = new int[n];
+            for (i = 0; i < n; i++)
+            {
+                intDistinct[i] = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    randomCount++;
+                    Random random = new Random();
+                    ran = random.Next(0, 11);
+                    if (ran == intDistinct[i])
+                    {
+                        Console.WriteLine("Random count:" + randomCount);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
